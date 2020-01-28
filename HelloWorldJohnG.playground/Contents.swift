13cb7d083a06a14513502e2fdfdf7ec68891c0d7
Ninja-Lombax <godzillakotm80@gmail.com>
2020-01-27 20:33:27 -0600

    Initial commit

---
+++ HelloWorldJohnG.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+print("Hello World")
+
+var name = "Jon"
+var language = "Swift"
+
+var message1 = "Welcome to the wonderful world of"
+var message2 = "\(name), Welcome to the wonderful world of \(language) !"
+
+print (message2)
+print (name, message1, language, "!")
+
+var name1 = "Jon"
+var name2 = "Kim"
+var name3 = "Kailey"
+var name4 = "Kara"
+var name5 = "Derek"
+
+var line = ""
+
+print (name1, name2, name3, name4, name5, separator:", ", terminator:"", to: &line)
+print (line)
+
+
+ 
